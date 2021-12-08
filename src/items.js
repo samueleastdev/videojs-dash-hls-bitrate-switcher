76723848f0a8bcd2b4d06906da6bf6aca05f85fb
S3bubble <support@s3bubble.com>
2021-12-08 17:28:30 +0000

    Auto selects the current rendition

--- src/items.js
+++ src/items.js
@@ -7,10 +7,10 @@ export class PlayBackRatesBtn extends MenuItem {
     super(player, options);
     this.height = options.height;
     this.levels = options.levels;
+    this.bitrate = options.bitrate;
     this.setAttribute('data-bitrate', options.bitrate);
   }
   handleClick(event) {
-
     // Add the selected class
     this.parentComponent_.children_.forEach(child => {
       child.removeClass('vjs-selected');
@@ -19,7 +19,6 @@ export class PlayBackRatesBtn extends MenuItem {
     this.addClass('vjs-selected');
 
     this.levels.forEach(level => {
-
       if (this.bitrate === level.bitrate) {
         level.enabled = true;
       } else {
